212c6ee0ac3ff1cc28ecc1849953a32dae9ce975
khititach <khititach58@email.nu.ac.th>
2020-03-10 16:37:19 +0700

    change mongodb url

--- config/key.js
+++ config/key.js
@@ -1,4 +1,4 @@
 module.exports = {
-    // MongoUrl: 'mongodb+srv://admin:admin@epaymentschool-arvvq.mongodb.net/epaymentschool?retryWrites=true&w=majority'
-    MongoUrl: 'mongodb://localhost:27017/epaymentprojectDB'
+    MongoUrl: 'mongodb+srv://admin:admin@epaymentschool-arvvq.mongodb.net/epaymentschool?retryWrites=true&w=majority'
+    // MongoUrl: 'mongodb://localhost:27017/epaymentprojectDB'
 }
