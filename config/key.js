136551ab0ae27c1e90c1142aac8ab7765d5f65a1
khititach <khititach58@email.nu.ac.th>
2020-03-10 08:46:15 +0700

    update key mongodb

--- config/key.js
+++ config/key.js
@@ -1,4 +1,5 @@
 module.exports = {
+    MongoUrl: 'mongodb+srv://admin:admin@epaymentschool-arvvq.mongodb.net/epaymentschool?retryWrites=true&w=majority'
     // MongoUrl: 'mongodb+srv://admin:admin@epayment-arvvq.gcp.mongodb.net/Epayment?retryWrites=true&w=majority'
-    MongoUrl: 'mongodb://localhost:27017/epaymentprojectDB'
+    // MongoUrl: 'mongodb://localhost:27017/epaymentprojectDB'
 }
